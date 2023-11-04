1c057147611433429a544e56a52eea2a00180656
saeed <saeednokarizi@gmail.com>
2023-11-04 18:56:09 +0330

    Add unit test for app.py

---
+++ unittest.py
@@ -0,0 +1,18 @@
+import unittest
+from app import app # assuming your Flask app is named app
+
+class BasicTestCase(unittest.TestCase):
+
+    def test_home(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about(self):
+        tester = app.test_client(self)
+        response = tester.get('/about', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
+
